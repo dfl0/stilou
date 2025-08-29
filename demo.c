4cbbb70aea2e96ea865711ffd7ad1e7fcd56e8ab
Dorin Flocos <dflocos@gmail.com>
2025-08-28 21:47:25 -0400

    update demo

--- demo.c
+++ demo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdint.h>
+#include <time.h>
 #include <SDL2/SDL.h>
 
 #define STILOU_IMPLEMENTATION
@@ -10,51 +12,108 @@
 
 uint32_t pixels[HEIGHT*WIDTH] = {0};
 
-void render_canvas(Stilou_Canvas canvas) {
+void init_sdl() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         exit(1);
     }
+}
 
-    SDL_Window *window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+SDL_Window *init_window() {
+    SDL_Window *window = SDL_CreateWindow("stilou", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
     if (!window) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         exit(1);
     }
 
-    for (size_t y = 0; y < canvas.height; ++y) {
-        for (size_t x = 0; x < canvas.width; ++x) {
-            uint32_t color = canvas.pixels[y*canvas.width + x];
-            SDL_SetRenderDrawColor(renderer,
-                                   (color & 0xFF000000) >> (8*3),  // R
-                                   (color & 0x00FF0000) >> (8*2),  // G
-                                   (color & 0x0000FF00) >> (8*1),  // B
-                                   (color & 0x000000FF) >> (8*0)); // A
-            SDL_RenderDrawPoint(renderer, x, y);
-        }
+    return window;
+}
+
+SDL_Renderer *init_renderer(SDL_Window *window) {
+    return SDL_CreateRenderer(window, -1, 0);
+}
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+void render_canvas(Stilou_Canvas canvas) {
+    static SDL_Texture *texture = NULL;
+
+    if (!texture) {
+        texture = SDL_CreateTexture(renderer,
+                                    SDL_PIXELFORMAT_RGBA8888,
+                                    SDL_TEXTUREACCESS_STREAMING,
+                                    canvas.width, canvas.height);
     }
 
+    SDL_UpdateTexture(texture, NULL, canvas.pixels, canvas.width * sizeof(uint32_t));
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);
+}
 
-    int quit = 0;
+int should_quit() {
     SDL_Event event;
-    while (!quit) {
-        if (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = 1;
-            }
+    if (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            return 1;
         }
     }
+    return 0;
+}
 
+void quit_sdl() {
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
 int main(void) {
+    // initialize SDL, as well as the window and renderer
+    init_sdl();
+    window = init_window();  // TODO: maybe use canvas as input
+    renderer = init_renderer(window);
+
+    // create a stilou canvas
     Stilou_Canvas canvas = stilou_create_canvas(pixels, WIDTH, HEIGHT);
-    stilou_fill_canvas(canvas, 0xFF00FFFF);
-    render_canvas(canvas);
+
+    int target_fps = 60;
+    int nanos_per_frame = 1000*1000*1000/target_fps;
+
+    struct timespec t;
+
+    size_t rect_w = 150;
+    size_t rect_h = 100;
+    int x = canvas.width/2 - rect_w/2;
+    int y = canvas.height/2 - rect_h/2;
+
+    int x_speed = 5;
+    int y_speed = 5;
+
+    while (!should_quit()) {
+        clock_t frame_start = clock();
+
+        x += x_speed;
+        y += y_speed;
+        if ((size_t)x + rect_w > canvas.width || x < 0) x_speed *= -1;
+        if ((size_t)y + rect_h > canvas.height || y < 0) y_speed *= -1;
+
+        // start drawing
+        stilou_fill_canvas(canvas, 0x202020FF); // (clear screen)
+        stilou_fill_rect(canvas, x, y, rect_w, rect_h, 0xF02020FF);
+
+        // show the drawing
+        render_canvas(canvas);
+
+        // maintain a consistent framerate
+        int nanos_taken = (clock() - frame_start)*1000*1000*1000/CLOCKS_PER_SEC;
+        if (nanos_taken < nanos_per_frame) {
+            t.tv_sec = 0;
+            t.tv_nsec = nanos_per_frame - nanos_taken;
+            if (nanosleep(&t, NULL) < 0) exit(1);
+        }
+    }
+
+    quit_sdl();
     return 0;
 }
